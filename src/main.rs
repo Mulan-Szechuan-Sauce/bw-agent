4a04940f565beac454d86bb0d5bb9e3fedb17395
Josh Wolfe <joshuafwolfe@gmail.com>
2023-01-13 17:01:23 -0500

    Swap to using openssl signature to work with openssh, support passphrase keys

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use clap::Parser;
 use clap_serde_derive::ClapSerde;
 use hmac::{Hmac, Mac};
-use openssl::{symm::Cipher, hash::MessageDigest};
+use openssl::{hash::MessageDigest, symm::Cipher};
 use sha2::Sha256;
 use ssh_agent_lib::Agent;
 use ssh_key::{PrivateKey, PublicKey};
@@ -25,8 +25,9 @@ impl MasterKey {
             email.as_ref(),
             kdf_iterations,
             MessageDigest::sha256(),
-            &mut res
-        ).expect("Key derivation failed");
+            &mut res,
+        )
+        .expect("Key derivation failed");
         MasterKey(res)
     }
 
@@ -57,60 +58,65 @@ impl EncryptedThruple {
     }
 
     fn mac_verify(&self, mac_key: &[u8]) {
-        let mut mac_to_verify = Hmac::<Sha256>::new_from_slice(mac_key).expect("HMAC can take key of any size");
+        let mut mac_to_verify =
+            Hmac::<Sha256>::new_from_slice(mac_key).expect("HMAC can take key of any size");
         mac_to_verify.update(&self.iv);
         mac_to_verify.update(&self.ct);
         mac_to_verify.verify_slice(&self.mac).unwrap();
     }
 
     fn decrypt(&self, enc_key: &[u8]) -> Vec<u8> {
-        openssl::symm::decrypt(
-            Cipher::aes_256_cbc(),
-            &enc_key,
-            Some(&self.iv),
-            &self.ct,
-        ).expect("Decryption failed")
+        openssl::symm::decrypt(Cipher::aes_256_cbc(), &enc_key, Some(&self.iv), &self.ct)
+            .expect("Decryption failed")
     }
 }
 
 #[derive(Debug)]
 struct BwSshKeyEntry {
     key: PrivateKey,
-    pubkey: PublicKey,
     passphrase: Option<Vec<u8>>,
     name: String,
 }
 
 fn oath_login(config: &Config) -> BwLoginResponse {
     let client = reqwest::blocking::Client::builder()
-                                    .danger_accept_invalid_certs(config.ignore_untrusted_cert)
-                                    .build()
-                                    .unwrap();
+        .danger_accept_invalid_certs(config.ignore_untrusted_cert)
+        .build()
+        .unwrap();
 
     let device_uuid = Uuid::new_v4().to_string();
     let params = HashMap::from([
         ("grant_type", "client_credentials"),
         ("scope", "api"),
         ("client_id", config.oauth_client_id.as_ref().unwrap()),
-        ("client_secret", config.oauth_client_secret.as_ref().unwrap()),
+        (
+            "client_secret",
+            config.oauth_client_secret.as_ref().unwrap(),
+        ),
         ("device_identifier", &device_uuid),
         ("device_name", &device_uuid),
     ]);
 
-    let response = client.post(format!("{}/identity/connect/token", config.url))
+    let response = client
+        .post(format!("{}/identity/connect/token", config.url))
         .form(&params)
         .send()
         .expect("Login request failed");
 
-    response.json::<BwLoginResponse>().expect("Login json failed to deserialize")
+    response
+        .json::<BwLoginResponse>()
+        .expect("Login json failed to deserialize")
 }
 
-
-fn fetch_ssh_keys(config: &Config, master_password: &str, login_response: &BwLoginResponse) -> Vec<BwSshKeyEntry> {
+fn fetch_ssh_keys(
+    config: &Config,
+    master_password: &str,
+    login_response: &BwLoginResponse,
+) -> Vec<BwSshKeyEntry> {
     let client = reqwest::blocking::Client::builder()
-                                    .danger_accept_invalid_certs(config.ignore_untrusted_cert)
-                                    .build()
-                                    .unwrap();
+        .danger_accept_invalid_certs(config.ignore_untrusted_cert)
+        .build()
+        .unwrap();
 
     let master_key = MasterKey::new(
         config.email.as_bytes(),
@@ -129,63 +135,78 @@ fn fetch_ssh_keys(config: &Config, master_password: &str, login_response: &BwLog
     let data_key = &key[..32];
     let data_mac = &key[32..];
 
-    let response = client.get(format!("{}/api/sync", config.url))
+    let response = client
+        .get(format!("{}/api/sync", config.url))
         .bearer_auth(login_response.access_token.clone())
         .send()
         .expect("Sync request failed");
 
-    let sync_response = response.json::<BwSyncResponse>().expect("Sync json failed to deserialize");
+    let sync_response = response
+        .json::<BwSyncResponse>()
+        .expect("Sync json failed to deserialize");
 
-    let folder_id = sync_response.folders.into_iter().find_map(|folder| {
-        let enc_name = EncryptedThruple::from_str(&folder.name);
-        enc_name.mac_verify(data_mac);
+    let folder_id = sync_response
+        .folders
+        .into_iter()
+        .find_map(|folder| {
+            let enc_name = EncryptedThruple::from_str(&folder.name);
+            enc_name.mac_verify(data_mac);
 
-        if enc_name.decrypt(data_key) == b"ssh-keys" {
-            Some(folder.id)
-        } else {
-            None
-        }
-    }).expect("ssh-keys folder does not exist");
-
-    sync_response.ciphers.into_iter().filter_map(|cipher| {
-        let cipher = match (&cipher.t, &cipher.folder_id) {
-            (BwCipherType::Note, Some(fid)) if *fid == folder_id => Some(cipher),
-            _ => None,
-        }?;
-
-        let note = EncryptedThruple::from_str(&cipher.notes.unwrap());
-        note.mac_verify(&data_mac);
-
-        let unencrypted_ssh_key = note.decrypt(data_key);
-
-        let fields = cipher.data.fields.unwrap_or(vec![]);
-
-        let passphrase_field = extract_field(data_mac, data_key, &fields, b"passphrase");
-
-        let pubkey_bytes = extract_field(data_mac, data_key, &fields, b"pubkey").expect("Missing pubkey");
-        let pubkey = std::str::from_utf8(&pubkey_bytes).expect("Invalid UTF-8 entry name").to_owned() ;
-
-        let name_cipher = EncryptedThruple::from_str(&cipher.name);
-        name_cipher.mac_verify(data_mac);
-        let name_bytes = name_cipher.decrypt(data_key);
-        let name = std::str::from_utf8(&name_bytes).expect("Invalid UTF-8 entry name").to_owned();
-
-        Some(BwSshKeyEntry {
-            key: PrivateKey::from_openssh(unencrypted_ssh_key).expect("Failed to parse private key"),
-            pubkey: PublicKey::from_openssh(&pubkey).expect("Failed to parse public key"),
-            passphrase: passphrase_field,
-            name: name,
+            if enc_name.decrypt(data_key) == b"ssh-keys" {
+                Some(folder.id)
+            } else {
+                None
+            }
         })
-    }).collect()
+        .expect("ssh-keys folder does not exist");
+
+    sync_response
+        .ciphers
+        .into_iter()
+        .filter_map(|cipher| {
+            let cipher = match (&cipher.t, &cipher.folder_id) {
+                (BwCipherType::Note, Some(fid)) if *fid == folder_id => Some(cipher),
+                _ => None,
+            }?;
+
+            let note = EncryptedThruple::from_str(&cipher.notes.unwrap());
+            note.mac_verify(&data_mac);
+
+            let unencrypted_ssh_key = note.decrypt(data_key);
+
+            let fields = cipher.data.fields.unwrap_or(vec![]);
+
+            let passphrase_field = extract_field(data_mac, data_key, &fields, b"passphrase");
+
+            let name_cipher = EncryptedThruple::from_str(&cipher.name);
+            name_cipher.mac_verify(data_mac);
+            let name_bytes = name_cipher.decrypt(data_key);
+            let name = std::str::from_utf8(&name_bytes)
+                .expect("Invalid UTF-8 entry name")
+                .to_owned();
+
+            Some(BwSshKeyEntry {
+                key: PrivateKey::from_openssh(unencrypted_ssh_key)
+                    .expect("Failed to parse private key"),
+                passphrase: passphrase_field,
+                name: name,
+            })
+        })
+        .collect()
 }
 
-fn extract_field(data_mac: &[u8], data_key: &[u8], fields: &Vec<BwCipherField>, field_name: &[u8]) -> Option<Vec<u8>> {
+fn extract_field(
+    data_mac: &[u8],
+    data_key: &[u8],
+    fields: &Vec<BwCipherField>,
+    field_name: &[u8],
+) -> Option<Vec<u8>> {
     fields.iter().find_map(|field| {
         let enc_name = EncryptedThruple::from_str(&field.name);
         enc_name.mac_verify(&data_mac);
         let name = enc_name.decrypt(data_key);
 
-        if name == name {
+        if name == field_name {
             let enc_value = EncryptedThruple::from_str(&field.value);
             enc_value.mac_verify(&data_mac);
             Some(enc_value.decrypt(data_key))
@@ -195,79 +216,22 @@ fn extract_field(data_mac: &[u8], data_key: &[u8], fields: &Vec<BwCipherField>, 
     })
 }
 
-// fn write_mpint(buffer: &mut BytesMut, value: &MPInt) {
-//     let bytes = value.as_bytes();
-//     buffer.put_u32(bytes.len() as u32);
-//     buffer.put_slice(bytes);
-// }
-
-// fn send_keys_to_agent(keys: Vec<BwSshKeyEntry>) {
-//     let sock_path = std::env::var_os("SSH_AUTH_SOCK").expect("Socket path is not set");
-//     let mut stream = UnixStream::connect(sock_path).expect("Could not connect to ssh-agent socket");
-//
-//     for key in keys {
-//         let mut message = BytesMut::new();
-//         message.put_bytes(SshMessageType::AddIdentity as u8, 1);
-//
-//         message.put_u32(usize::try_into(SSH_RSA_KEY_TYPE.len()).unwrap());
-//         message.put_slice(SSH_RSA_KEY_TYPE);
-//
-//         let pk = PrivateKey::from_openssh(&key.key).unwrap();
-//         let pk = if pk.is_encrypted() {
-//             // TODO: Prompt for user input of key passphrase
-//             let passphrase = key.passphrase.unwrap_or_else(||
-//                 rpassword::prompt_password(format!("Key '{}' Password: ", key.name)).unwrap().into_bytes()
-//             );
-//             pk.decrypt(passphrase).expect("Invalid password")
-//         } else {
-//             pk
-//         };
-//
-//         let rsa_keypair = pk.key_data().rsa().expect("Only supports RSA now");
-//
-//         write_mpint(&mut message, &rsa_keypair.public.n);
-//         write_mpint(&mut message, &rsa_keypair.public.e);
-//         write_mpint(&mut message, &rsa_keypair.private.d);
-//         write_mpint(&mut message, &rsa_keypair.private.iqmp);
-//         write_mpint(&mut message, &rsa_keypair.private.p);
-//         write_mpint(&mut message, &rsa_keypair.private.q);
-//
-//         let comment = [b"BW: ", key.name.as_bytes()].concat();
-//         message.put_u32(usize::try_into(comment.len()).unwrap());
-//         message.put_slice(&comment);
-//
-//         let frozen = message.freeze();
-//
-//         let mut final_msg = BytesMut::new();
-//         final_msg.put_u32(u32::try_from(frozen.len()).unwrap());
-//         final_msg.put_slice(&frozen);
-//
-//         stream.write_all(&final_msg.freeze()).unwrap();
-//
-//         let mut size_bytes = vec![0u8; 4];
-//         stream.read_exact(&mut size_bytes).unwrap();
-//
-//         let _size = BigEndian::read_u32(&size_bytes);
-//     }
-//
-//     stream.shutdown(Shutdown::Both).expect("shutdown function failed");
-// }
-
 fn password_login(config: &Config, master_password: &str) -> BwLoginResponse {
     let client = reqwest::blocking::Client::builder()
-                                    .danger_accept_invalid_certs(config.ignore_untrusted_cert)
-                                    .build()
-                                    .unwrap();
+        .danger_accept_invalid_certs(config.ignore_untrusted_cert)
+        .build()
+        .unwrap();
 
-    let prelogin_params = HashMap::from([
-        ("email", &config.email),
-    ]);
-    let prelogin_response = client.post(format!("{}/api/accounts/prelogin", config.url))
+    let prelogin_params = HashMap::from([("email", &config.email)]);
+    let prelogin_response = client
+        .post(format!("{}/api/accounts/prelogin", config.url))
         .json(&prelogin_params)
         .send()
         .expect("Pre login request failed");
 
-    let prelogin = prelogin_response.json::<BwPreloginResponse>().expect("Preflight json failed to deserialize");
+    let prelogin = prelogin_response
+        .json::<BwPreloginResponse>()
+        .expect("Preflight json failed to deserialize");
 
     let master_key = MasterKey::new(
         config.email.as_bytes(),
@@ -281,8 +245,9 @@ fn password_login(config: &Config, master_password: &str) -> BwLoginResponse {
         master_password.as_bytes(),
         1,
         MessageDigest::sha256(),
-        &mut hashed
-    ).expect("Failed to hash user master password");
+        &mut hashed,
+    )
+    .expect("Failed to hash user master password");
 
     let encoded = base64::encode(hashed);
 
@@ -298,50 +263,57 @@ fn password_login(config: &Config, master_password: &str) -> BwLoginResponse {
         ("password", encoded),
     ]);
 
-    let mut response = client.post(format!("{}/identity/connect/token", config.url))
+    let mut response = client
+        .post(format!("{}/identity/connect/token", config.url))
         .form(&params)
         .send()
         .expect("Login request failed");
 
     if response.status() == 400 {
-        let totp_response = response.json::<BwTwoFactorResponse>().expect("2FA json failed to deserialize");
+        let totp_response = response
+            .json::<BwTwoFactorResponse>()
+            .expect("2FA json failed to deserialize");
 
         match *totp_response.two_factor_providers.first().unwrap() {
             // TOTP or Yubikey TOTP
-            id@(0 | 1 | 3) => {
+            id @ (0 | 1 | 3) => {
                 let prompt = match id {
                     0 => "TOTP",
                     1 => "Emailed Code",
                     3 => "Yubikey OTP",
-                    _ => unreachable!("Rust isn't smart enough to know these are the only valid cases"),
+                    _ => unreachable!(
+                        "Rust isn't smart enough to know these are the only valid cases"
+                    ),
                 };
 
                 let totp = rpassword::prompt_password(format!("{}: ", prompt)).unwrap();
                 params.insert("twoFactorToken", totp);
                 params.insert("twoFactorProvider", id.to_string());
                 params.insert("twoFactorRemember", "0".to_owned()); // TODO: Don't be an old man
-            },
+            }
             // FIDO
-            7 => {
-            },
+            7 => {}
             _ => panic!("Unsupported 2FA type"),
         }
-        response = client.post(format!("{}/identity/connect/token", config.url))
+        response = client
+            .post(format!("{}/identity/connect/token", config.url))
             .form(&params)
             .send()
             .expect("Login request failed");
-
     }
 
-    response.json::<BwLoginResponse>().expect("Login json failed to deserialize")
+    response
+        .json::<BwLoginResponse>()
+        .expect("Login json failed to deserialize")
 }
 
 fn main() {
     let mut cli_args = Args::parse();
 
     let config = match std::fs::read_to_string(&cli_args.config) {
-        Ok(x) => Config::from(serde_yaml::from_str::<<Config as ClapSerde>::Opt>(&x).unwrap()).merge(&mut cli_args.user_config),
-        Err(_) => Config::from(&mut cli_args.user_config)
+        Ok(x) => Config::from(serde_yaml::from_str::<<Config as ClapSerde>::Opt>(&x).unwrap())
+            .merge(&mut cli_args.user_config),
+        Err(_) => Config::from(&mut cli_args.user_config),
     };
 
     let master_password = rpassword::prompt_password("Master Password: ").unwrap();
@@ -350,8 +322,6 @@ fn main() {
     } else {
         password_login(&config, &master_password)
     };
-
-    // let bw_ssh_keys = fetch_ssh_keys(&config, &master_password, login);
 
     let agent = BwSshAgent {
         config,
@@ -362,8 +332,4 @@ fn main() {
     let _ = std::fs::remove_file(socket);
 
     agent.run_unix(socket).expect("Failed to run socket");
-
-    // send_keys_to_agent(bw_ssh_keys);
-    // println!("Successfully added keys.");
 }
-
--- src/ssh_agent.rs
+++ src/ssh_agent.rs
@@ -1,6 +1,14 @@
+use std::error::Error;
+
+use openssl::bn::BigNum;
+use openssl::hash::MessageDigest;
+use openssl::pkey::{PKey, Private};
+use openssl::rsa::Rsa;
+use openssl::sign::Signer;
 use ssh_agent_lib::agent::Agent;
 use ssh_agent_lib::proto::message::Message;
-use ssh_agent_lib::proto::{Identity, Signature, Blob};
+use ssh_agent_lib::proto::{Identity, Signature};
+use ssh_key::{PrivateKey, SigningKey};
 
 use crate::fetch_ssh_keys;
 use crate::types::{BwLoginResponse, Config};
@@ -18,51 +26,89 @@ impl Agent for BwSshAgent {
         let bw_ssh_keys = fetch_ssh_keys(&self.config, &self.master_password, &self.login);
 
         match message {
-            Message::RequestIdentities => {
-                Ok(Message::IdentitiesAnswer(
-                    bw_ssh_keys
-                        .iter()
-                        .map(|key| Identity {
-                            pubkey_blob: key.pubkey.to_bytes().unwrap(),
-                            comment: key.name.clone(),
-                        })
-                        .collect(),
-                ))
-            },
+            Message::RequestIdentities => Ok(Message::IdentitiesAnswer(
+                bw_ssh_keys
+                    .iter()
+                    .map(|key| Identity {
+                        pubkey_blob: key.key.public_key().to_bytes().unwrap(),
+                        comment: key.name.clone(),
+                    })
+                    .collect(),
+            )),
             Message::SignRequest(request) => {
-                let key = bw_ssh_keys.iter().find(|key| {
-                    key.pubkey.to_bytes().unwrap() == request.pubkey_blob
-                });
+                let key = bw_ssh_keys
+                    .iter()
+                    .find(|key| key.key.public_key().to_bytes().unwrap() == request.pubkey_blob);
 
                 if let Some(k) = key {
-                    let (hash_alg, algorithm) =  if request.flags & 4 != 0 {
-                        (ssh_key::HashAlg::Sha512, "rsa-sha2-512")
+                    let (digest, algorithm) = if request.flags & 4 != 0 {
+                        (MessageDigest::sha512(), "rsa-sha2-512")
                     } else if request.flags & 2 != 0 {
-                        (ssh_key::HashAlg::Sha256, "rsa-sha2-256")
+                        (MessageDigest::sha256(), "rsa-sha2-256")
                     } else {
-                        return Ok(Message::Failure)
+                        (MessageDigest::sha1(), "ssh-rsa")
                     };
 
-                    let signed = k.key.sign("namespace.fixme", hash_alg, &request.data).expect("Thingy");
+                    // TODO: Write this nicer. Get rid of the un-needed clone
+                    let decrypted_key = if k.key.is_encrypted() {
+                        k.key
+                            .decrypt(
+                                k.passphrase
+                                    .as_ref()
+                                    .expect("Key is encrypted yet no passphrase was provided"),
+                            )
+                            .expect("Unabled to decrypt key with passphrase")
+                    } else {
+                        k.key.to_owned()
+                    };
+
+                    let keypair = PKey::from_rsa(
+                        rsa_openssl_from_ssh(&decrypted_key).expect("Failed to convert key"),
+                    )
+                    .expect("Failed to construct PKey");
+                    let mut signer =
+                        Signer::new(digest, &keypair).expect("Failed to create signed");
+                    signer
+                        .update(&request.data)
+                        .expect("Failed to update signer");
+
                     let signature = Signature {
                         algorithm: algorithm.to_owned(),
-                        blob: signed.signature_bytes().to_owned(),
+                        blob: signer
+                            .sign_to_vec()
+                            .expect("Failed to convert signed to vec"),
                     };
 
-                    Ok(Message::SignResponse(signature.to_blob().expect("Blobify failed")))
+                    Ok(Message::SignResponse(
+                        ssh_agent_lib::proto::to_bytes(&signature)
+                            .expect("Failed to convert signature to bytes"),
+                    ))
                 } else {
                     Ok(Message::Failure)
                 }
-
-            },
+            }
             Message::Extension(request) if request.extension_type == "session-bind@openssh.com" => {
                 /* This seems to be optional to implement */
                 Ok(Message::ExtensionFailure)
-            },
+            }
             msg => {
                 dbg!(msg);
                 Ok(Message::Failure)
-            },
+            }
         }
     }
+}
+
+fn rsa_openssl_from_ssh(private_key: &PrivateKey) -> Result<Rsa<Private>, Box<dyn Error>> {
+    let data = private_key.key_data().rsa().expect("Only support rsa keys");
+    let n = BigNum::from_slice(data.public.n.as_bytes())?;
+    let e = BigNum::from_slice(data.public.e.as_bytes())?;
+    let d = BigNum::from_slice(data.private.d.as_bytes())?;
+    let qi = BigNum::from_slice(data.private.iqmp.as_bytes())?;
+    let p = BigNum::from_slice(data.private.p.as_bytes())?;
+    let q = BigNum::from_slice(data.private.q.as_bytes())?;
+    let dp = &d % &(&p - &BigNum::from_u32(1)?);
+    let dq = &d % &(&q - &BigNum::from_u32(1)?);
+
+    Ok(Rsa::from_private_components(n, e, d, p, q, dp, dq, qi)?)
 }
